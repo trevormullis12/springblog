d2803d4328df0b6adbb7d5fc96e95d7cdc3256df
trevormullis12 <trevor.mullis12@gmail.com>
2020-08-03 10:56:49 -0500

    finished views exercise

--- src/main/java/com/codeup/springblog/DiceController.java
+++ src/main/java/com/codeup/springblog/DiceController.java
@@ -1,7 +1,10 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DiceController {
@@ -9,6 +12,17 @@ public class DiceController {
 
     @GetMapping("/roll-dice")
     public String dice() {
-        return "";
+        return "dice";
+    }
+
+    @GetMapping("/roll-dice/{number}")
+    @ResponseBody
+    public String rolled(@PathVariable int number, Model model) {
+        int randomNum = (int) Math.floor((Math.random() * 5) + 1);
+        if(number == randomNum) {
+            return "You rolled a " + randomNum + " and guessed " + number + ", you guessed correctly!";
+        } else {
+            return "You rolled a " + randomNum + " and guessed " + number + ", you guessed incorrectly...";
+        }
     }
 }
